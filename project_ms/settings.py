26be322da96f5033b780f1f71456601ccbbef097
Owenn Gimli <>
2021-07-08 10:06:06 +0800

    added crispy forms

--- project_ms/settings.py
+++ project_ms/settings.py
@@ -27,6 +27,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'crispy_forms',
+    'crispy_bootstrap5',
+
     'project.apps.ProjectConfig',
 ]
 
@@ -120,3 +123,7 @@ STATICFILES_DIR = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
